c3339f6dca729b655e20d6bd5aaaaa94b9c9a04f
General-Monish <monissam28@gmail.com>
2024-01-16 23:31:58 +0530

    Added StateMachine For different Player States

---
+++ Scripts/GameInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerinputActions;
+
+    private void Awake()
+    {
+        playerinputActions = new PlayerInputActions();
+        playerinputActions.Player.Move.Enable();
+    }
+
+    public Vector2 GetMovementNormalized()
+    {
+        Vector2 moveInput = playerinputActions.Player.Move.ReadValue<Vector2>();
+        moveInput = moveInput.normalized;
+        return moveInput;
+    }
+
+}
---
+++ Scripts/PlayerAttackingState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackingState : PlayerBaseState
+{
+    public override void EnterState(PlayerStateManager player)
+    {
+        
+    }
+
+    public override void FixedUpdateState(PlayerStateManager player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnCollisionEnter(PlayerStateManager player, Collision collision)
+    {
+        
+    }
+
+    public override void UpdateState(PlayerStateManager player)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
