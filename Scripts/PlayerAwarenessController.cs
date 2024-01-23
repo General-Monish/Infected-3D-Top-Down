24170a780db2521557f9eb6c824c077625e69289
General-Monish <monissam28@gmail.com>
2024-01-24 01:38:40 +0530

    Added enemy scripts

---
+++ Scripts/PlayerAwarenessController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAwarenessController : MonoBehaviour
+{
+    public static PlayerAwarenessController instance { get; private set; }
+    [HideInInspector]
+    public bool AwarenessOfPlayer;
+    [HideInInspector]
+    public Vector2 DirToPlayer;
+   [SerializeField] private float playerAwarenessDistance;
+    private Transform player;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+        player = FindObjectOfType<PlayerStateManager>().transform;
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemyToPlayerVecttor = player.position - transform.position;
+        DirToPlayer = enemyToPlayerVecttor.normalized;
+
+        if (enemyToPlayerVecttor.magnitude <= playerAwarenessDistance)
+        {
+            AwarenessOfPlayer = true;
+        }
+        else
+        {
+            AwarenessOfPlayer = false;
+        }
+    }
+}
