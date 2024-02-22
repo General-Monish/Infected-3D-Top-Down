afab6dd91191e41f805481dd2d0f0df2955a2971
General-Monish <monissam28@gmail.com>
2024-02-22 23:17:19 +0530

    added level1 and camera follow script

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;  // player's Transform
+    public float smoothSpeed = 0.125f;  //  the smoothness of the camera follow
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + new Vector3(0, 0, -10);  //  Z value based on your scene
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
--- Scripts/P.cs
+++ Scripts/P.cs
@@ -8,7 +8,8 @@ public class P : MonoBehaviour
     private Vector3 mousePosition;
     private NavMeshAgent agent;
     private Animator anim;
-   [SerializeField] private float rotatespeed=10f;
+    private bool isClicked = false;
+    //[SerializeField] private float rotatespeed = 10f;
 
     private void Awake()
     {
@@ -35,7 +36,6 @@ public class P : MonoBehaviour
         MouseMovement();
         SetDestination();
         rotate();
-        
     }
 
     private void MouseMovement()
@@ -45,23 +45,38 @@ public class P : MonoBehaviour
             anim.SetBool("walk", true);
             mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = transform.position.z;
+            isClicked = true;
         }
 
-        // Check if the player is close to the target position, and reset movement input
-        if (Vector2.Distance(transform.position, mousePosition) < 1f)
+        //  player is close to the target position, and reset movement input
+        if (Vector2.Distance(transform.position, mousePosition) < .1f)
         {
             anim.SetBool("walk", false);
+            isClicked = false;
         }
     }
 
     private void SetDestination()
     {
-        agent.SetDestination(mousePosition);
+        if (isClicked)
+        {
+            agent.SetDestination(mousePosition);
+        }
     }
 
     void rotate()
     {
-        transform.up = Vector2.Lerp(transform.forward, mousePosition, Time.deltaTime * rotatespeed);
+        Vector3 lookDirection = mousePosition - transform.position;
+        float distanceToTarget = Vector2.Distance(transform.position, mousePosition);
+
+        // if the player is close to the target
+        if (distanceToTarget > 1f)
+        {
+            float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+            // Use LookAt to set the rotation smoothly
+            transform.up = lookDirection.normalized;
+        }
     }
 
 }
