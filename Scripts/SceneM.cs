93299ebe2b5e1805922bd541f68b2fd09bf6a5ec
General-Monish <monissam28@gmail.com>
2024-04-24 15:52:02 +0530

    added MainMenu Scene

---
+++ Scripts/SceneM.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneM : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenuPanel;
+    [SerializeField]
+    private GameObject creditsPanel;
+    [SerializeField]
+    private GameObject SettingsPanel;
+
+    private void Start()
+    {
+        mainMenuPanel.SetActive(true);
+    }
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void BackButtonToMainMenu()
+    {
+        SettingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void CreditsButton()
+    {
+        creditsPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void SettingsButton()
+    {
+        SettingsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
